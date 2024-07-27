f6102abb50250b4eb998fd9dbcc88e5cef40b6eb
gaba65 <opsgabi65@gmail.com>
2024-07-27 15:25:18 +0300

    B: Added character counter feature

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ def main():
     bookpath = "books/frankestein.txt"
     text = getbooktext(bookpath)
     counter = counterwords(text)
-    print(text)
-    print(f"Count of words: {counter}")
-
+    counterch = countercharacters(text)
+    print(counterch)
 
 def getbooktext(path):
     with open(path) as f:
@@ -17,4 +16,19 @@ def counterwords(text):
     for word in words:
         counter += 1
     return(counter)
-main()
+
+def countercharacters(text):
+    countofcharacters = {
+        "a": 0, "b": 0, "c":0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, 
+        "k":0, "l":0, "m":0, "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0,
+        "u":0, "v":0, "w":0, "x":0, "y":0, "z":0
+    }
+    low = text.lower()
+    
+    for character in low:
+        if character in countofcharacters:
+            countofcharacters[character] += 1
+
+    return countofcharacters
+
+main()      
